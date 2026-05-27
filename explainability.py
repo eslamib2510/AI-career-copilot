9dd2d73f529bf2d0fff511c9daabea31840a4746
Eslam Ibrahim <eslamib2510@gmail.com>
2026-05-27 14:38:01 +0300

    Remove explainability dataset dependency

--- explainability.py
+++ explainability.py
@@ -1,43 +1,81 @@
-from data_loader import jobs_df
 
 # ====================================
-# EXPLAIN RECOMMENDATIONS
+# JOB SKILLS DATABASE
+# ====================================
+
+job_skills = {
+
+    "Machine Learning Engineer": [
+
+        "Python",
+        "Machine Learning",
+        "Deep Learning",
+        "TensorFlow",
+        "NLP",
+        "AI"
+    ],
+
+    "Data Scientist": [
+
+        "Python",
+        "SQL",
+        "Statistics",
+        "Machine Learning",
+        "Data Analysis"
+    ],
+
+    "AI Engineer": [
+
+        "Python",
+        "PyTorch",
+        "Transformers",
+        "Deep Learning",
+        "NLP"
+    ],
+
+    "Backend Developer": [
+
+        "Python",
+        "FastAPI",
+        "APIs",
+        "SQL",
+        "Backend Development"
+    ],
+
+    "Data Analyst": [
+
+        "Excel",
+        "SQL",
+        "Power BI",
+        "Statistics",
+        "Data Visualization"
+    ]
+}
+
+# ====================================
+# EXPLAINABLE AI
 # ====================================
 
 def explain_recommendation(
 
-    detected_skills,
+    user_skills,
 
     job_title
 ):
 
-    matched_skills = []
+    required_skills = job_skills.get(
+        job_title,
+        []
+    )
 
-    # Get matching jobs
-    matched_jobs = jobs_df[
-        jobs_df["title"] == job_title
+    matched_skills = [
+
+        skill
+
+        for skill in user_skills
+
+        if skill in required_skills
     ]
 
-    # Extract job skills
-    job_skills = set()
-
-    for _, row in matched_jobs.iterrows():
-
-        skill = row["skill_name"]
-
-        if isinstance(skill, str):
-
-            job_skills.add(
-                skill.lower()
-            )
-
-    # Compare with resume skills
-    for skill in detected_skills:
-
-        if skill.lower() in job_skills:
-
-            matched_skills.append(
-                skill
-            )
-
     return matched_skills
+
